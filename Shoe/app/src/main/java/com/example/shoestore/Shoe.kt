fee420509f76058e0db079f7b1c4e02e82f52baa
My AnFeVaGa <Job.andresvalencia@gmail.com>
2021-11-08 22:39:59 -0500

    Application in process.

---
+++ Shoe/app/src/main/java/com/example/shoestore/Shoe.kt
@@ -0,0 +1,10 @@
+package com.example.shoestore
+
+class Shoe {
+
+    var brand = ""
+    var image = ""
+
+    val shoes = ArrayList<Store>()
+
+}
---
+++ Shoe/app/src/main/java/com/example/shoestore/Store.kt
@@ -0,0 +1,22 @@
+package com.example.shoestore
+
+import java.util.ArrayList
+
+class Store {
+
+    val store = ArrayList<Shoe>()
+
+    private fun addToCart() {
+
+    }
+
+    private fun buy() {
+
+    }
+
+    private fun clean() {
+
+    }
+
+}
+
