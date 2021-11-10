7df0c23857e3b6604ccc8b621f46445a702d660a
My AnFeVaGa <Job.andresvalencia@gmail.com>
2021-11-10 08:10:27 -0500

    Application in process.

--- Shoe/app/src/main/java/com/example/shoestore/Shoe.kt
+++ Shoe/app/src/main/java/com/example/shoestore/Shoe.kt
@@ -5,6 +5,4 @@ class Shoe {
     var brand = ""
     var image = ""
 
-    val shoes = ArrayList<Store>()
-
 }
--- Shoe/app/src/main/java/com/example/shoestore/Store.kt
+++ Shoe/app/src/main/java/com/example/shoestore/Store.kt
@@ -4,7 +4,7 @@ import java.util.ArrayList
 
 class Store {
 
-    val store = ArrayList<Shoe>()
+    private val shoes = ArrayList<Shoe>()
 
     private fun addToCart() {
 
@@ -18,5 +18,38 @@ class Store {
 
     }
 
-}
+    private fun createShoeList() {
+        val shoeNike = Shoe()
+        shoeNike.brand = "nike"
+        shoeNike.image = ""
+        shoes.add(shoeNike)
 
+        val shoeAdidas = Shoe()
+        shoeAdidas.brand = "adidas"
+        shoeAdidas.image = ""
+        shoes.add(shoeAdidas)
+
+        val shoeReebok = Shoe()
+        shoeReebok.brand = "reebok"
+        shoeReebok.image = ""
+        shoes.add(shoeReebok)
+
+        val shoePolo = Shoe()
+        shoePolo.brand = "polo"
+        shoePolo.image = ""
+        shoes.add(shoeReebok)
+
+        val shoeJordan = Shoe()
+        shoeJordan.brand = "jordan"
+        shoeJordan.image = ""
+        shoes.add(shoeJordan)
+
+        val shoeNewBalance = Shoe()
+        shoeNewBalance.brand = "newbalance"
+        shoeNewBalance.image = ""
+        shoes.add(shoeNewBalance)
+
+    }
+
+
+}
